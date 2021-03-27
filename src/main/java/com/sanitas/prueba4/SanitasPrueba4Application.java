abfd4302c0f699cdeef11b299580adee05d05310
acastilm <acastilmail.com>
2021-03-27 09:27:10 +0100

    Primer commit con el esqueleto del proyecto Calculadora de Sanitas-Prueba4  con SpringBoot + Maven

---
+++ src/main/java/com/sanitas/prueba4/SanitasPrueba4Application.java
@@ -0,0 +1,13 @@
+package com.sanitas.prueba4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanitasPrueba4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanitasPrueba4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanitas/prueba4/SanitasPrueba4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanitas.prueba4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanitasPrueba4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
